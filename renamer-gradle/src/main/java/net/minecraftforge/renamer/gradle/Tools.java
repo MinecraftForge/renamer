553ca6f2166c259f6e9bd9502499d0b19c9b1cd4
LexManos <LexManos@gmail.com>
2026-04-06 09:12:06 -0700

    [gradle] Bump tool to 2.1.0, Uses upstream lambda detection, and fixes more cases for naive srg

--- renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/Tools.java
+++ renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/Tools.java
@@ -9,6 +9,6 @@ import net.minecraftforge.gradleutils.shared.Tool;
 final class Tools {
     private Tools() { }
 
-    static final Tool RENAMER = Tool.ofForge("classes", "net.minecraftforge:renamer:2.0.11:all", 8);
+    static final Tool RENAMER = Tool.ofForge("classes", "net.minecraftforge:renamer:2.1.0:all", 8);
     static final Tool SRG2SRC = Tool.ofForge("srg2source", "net.minecraftforge:Srg2Source:8.2.1:fatjar", 17);
 }
